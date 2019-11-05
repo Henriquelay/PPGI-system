1de64b8b3091c9a4db83f2b32eb5d134dbd5452f
Henrique Layber <henriquelayber@gmail.com>
2019-11-05 11:55:55 -0300

    Vamo

--- Sistema PPGI/src/Regra.java
+++ Sistema PPGI/src/Regra.java
@@ -1,3 +1,25 @@
-public class Regra {
+import java.util.Date;
+import java.util.Map;
 
+public class Regra implements Comparable {
+    private int anosAvaliados;
+    private float multPeriodicos;
+    private float pontuacaoMinima;
+    private Date dataInicio;
+    private Date dataFinal;
+    private Map<String,Integer> pontos;
+
+    @Override public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override public String toString() {
+        String str = "Data Início: " + this.dataInicio.toString() + " Data final: " + this.dataFinal.toString() + "\nPontuação mínima: " + this.pontuacaoMinima + "\nMultiplicador de periódicos: " + this.multPeriodicos + "\nAnos Avaliados: " + this.anosAvaliados + "\nPontos: ";
+
+        for(Map.Entry<String,Integer> e : this.pontos.entrySet()) {
+            str.concat(e.getKey() + ":" + e.getValue());
+        }
+        return str;
+    }
 }
