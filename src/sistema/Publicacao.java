1c782ca2ded5a108c61dab59ba1cd3d0ca31227f
Henriquelay <henriquelayber@gmail.com>
2019-11-07 02:33:43 -0300

    Ops esqueci de um rs

--- src/sistema/Publicacao.java
+++ src/sistema/Publicacao.java
@@ -1,5 +1,44 @@
 package sistema;
 
 public class Publicacao {
+    private int ano;
+    private String título;
+    private int numero;
 
+    // Getters e Setters
+    public int getAno() {return this.ano;}
+    public String getTítulo() {return this.título;}
+    public void setAno(int ano) {this.ano = ano;}
+    private int getNumero() {return this.numero;}
+    private void setTítulo(String título) {this.título = título;}
+    private void setNumero(int numero) {this.numero = numero;}
+
+    // To compare with standard function
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Publicacao)) {
+            return false;
+        }
+        Publicacao publicacao = (Publicacao) o;
+        return ano == publicacao.ano && Objects.equals(título, publicacao.título) && numero == publicacao.numero;
+    }
+
+    // To print with standard function
+    @Override
+    public String toString() {
+        return "{" +
+            " ano='" + getAno() + "'" +
+            ", título='" + getTítulo() + "'" +
+            ", numero='" + getNumero() + "'" +
+            "}";
+    }
+
+    // Constructor
+    public Publicacao(int ano, String título, int numero) {
+        this.setAno(ano);
+        this.setTítulo(título);
+        this.setNumero(numero);
+    }
 }
